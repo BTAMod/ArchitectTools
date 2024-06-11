4c291a142270384bad041651d41ad50bb178fcb4
kill05 <vinkimeme@libero.it>
2024-06-11 14:24:47 +0200

    Added hoe functionality to ArchitectMattock

--- src/main/java/com/github/kill05/items/tool/ArchitectMattock.java
+++ src/main/java/com/github/kill05/items/tool/ArchitectMattock.java
@@ -3,6 +3,11 @@ package com.github.kill05.items.tool;
 import com.github.kill05.items.part.ArchitectPart;
 import com.github.kill05.items.part.statistics.PartStatistic;
 import net.minecraft.core.block.tag.BlockTags;
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
 
 public class ArchitectMattock extends ArchitectTool {
 
@@ -19,5 +24,10 @@ public class ArchitectMattock extends ArchitectTool {
 		addMineableTags(BlockTags.MINEABLE_BY_AXE);
 		addMineableTags(BlockTags.MINEABLE_BY_SHOVEL);
 		addMineableTags(BlockTags.MINEABLE_BY_HOE);
+	}
+
+	@Override
+	public boolean onItemUse(ItemStack itemstack, EntityPlayer entityplayer, World world, int blockX, int blockY, int blockZ, Side side, double xPlaced, double yPlaced) {
+		return Item.toolHoeDiamond.onItemUse(itemstack, entityplayer, world, blockX, blockY, blockZ, side, xPlaced, yPlaced);
 	}
 }
