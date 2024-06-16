3cd1026d423de8fd53fbb84dfcb739411f5bd111
kill05 <vinkimeme@libero.it>
2024-06-16 19:37:56 +0200

    Shortswords now disable mining in creative

--- src/main/java/com/github/kill05/items/tool/ArchitectShortsword.java
+++ src/main/java/com/github/kill05/items/tool/ArchitectShortsword.java
@@ -3,6 +3,7 @@ package com.github.kill05.items.tool;
 import com.github.kill05.items.part.ArchitectPart;
 import com.github.kill05.items.part.statistics.PartStatistic;
 import net.minecraft.core.block.tag.BlockTags;
+import net.minecraft.core.item.tag.ItemTags;
 
 public class ArchitectShortsword extends ArchitectTool {
 
@@ -14,8 +15,9 @@ public class ArchitectShortsword extends ArchitectTool {
 		addPart(ArchitectPart.TOOL_ROD, "shortsword/handle").renderPriority(1);
 
 		addValidStatistic(PartStatistic.ENTITY_DAMAGE, 1.1f);
-
 		addMineableTags(BlockTags.MINEABLE_BY_SWORD);
+		//noinspection unchecked
+		withTags(ItemTags.PREVENT_CREATIVE_MINING);
 	}
 
 
